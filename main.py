680f66ef651a03e454263da58b34796f090445d2
Denis <icestrelok@gmail.com>
2024-07-24 22:45:07 +0300

    Creted def: select_proxy, cange_type

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-print("Hello Wold")
-Print("Test")
+# proxy6.net
+import dotenv  #pip install python-dotenv
+from os import environ
+import urllib.request,json
+
+
+dotenv.load_dotenv('.env')
+
+api_key  = environ['proxy6_api_key']
+url = f"https://proxy6.net/api/{api_key}/getproxy"
+data = "" # Все данные по этому API_KEY
+proxy_list = list() # инфо о прокси
+selected_idx = 0 # Выбранный номер Прокси из списка
+command = 'nodeta' # комманда, какое действие выполнить с прокси
+
+# считать все данные данные
+with urllib.request.urlopen(url) as u:
+    data = dict(json.load(u))
+
+
+# получаем  данные только для Прокси
+for proxy in data["list"]:
+    proxy_list.append(data["list"][proxy])
+
+
+def select_proxy():
+    print("Выберите Прокси")
+    idx = 1
+    for prx in proxy_list:
+        print(
+            f"{idx}: IP:{prx['ip']} HOST:{prx['host']} PORT:{prx['port']} USER:{prx['user']} PASS:{prx['pass']} ТИП:{prx['type']} IPv:{prx['version']} ДЕЙСТВУЕТ_ДО:{prx['date_end']}")
+        idx = idx + 1
+    print("Назад: 0")
+    return int(input("Введите номер:> "))
+
+def cange_type(api_key, ids, proxy_type="https"):
+    # запрос на изменение Типа Прокси Socks/HTTPS
+    # proxy_type = "https" # "https" / "socks"
+    req = f"https://proxy6.net/api/{api_key}/settype?ids={ids}&type={proxy_type}"
+    with urllib.request.urlopen(req) as u:
+        d = json.load(u)
+        print(d)
